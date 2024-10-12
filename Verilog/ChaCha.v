a4fd3f69b062f7962d869b1f9ba2bd795e2da3ee
akhil <akhil.s2004@protonmail.com>
2024-10-12 21:15:21 +0530

    Removed partial modules

--- Verilog/ChaCha.v
+++ Verilog/ChaCha.v
@@ -226,7 +226,7 @@ module main (
     // Internal signals
     wire [1:0] counter_output;
     wire [3:0] ksg_output1, ksg_output2, ksg_output3, ksg_output4;
-    wire [3:0] constant = 4'b1101;
+    wire [3:0] constant = 4'b1011;
 
     wire bit_selector_output;
     wire plain_text_flag;
@@ -275,73 +275,4 @@ module main (
     // XOR the bit selector output with the plain-text bit value
     assign final_output = bit_selector_output ^ plain_text_bit_value;
 
-endmodule
-
-
-//testbench for main module
-module main_tb;
-    // Inputs
-    reg clk;
-    reg reset;
-    reg [7:0] key;
-    reg [1:0] nonce;
-    reg  plain_text_input1;
-    reg plain_text_input2;
-    reg lock;
-    reg [1:0] init_value;
-
-    // Outputs
-    wire final_output;
-
-    // Instantiate the main module
-    main uut (
-        .clk(clk),
-        .reset(reset),
-        .key(key),
-        .nonce(nonce),
-        .plain_text_input1(plain_text_input1),
-        .plain_text_input2(plain_text_input2),
-        .init_value(init_value),
-        .lock(lock),
-        .final_output(final_output)
-    );
-
-    // Clock generation
-    initial begin
-        clk = 0;
-        forever #5 clk = ~clk; // 10ns period clock
-    end
-
-    // Test sequence
-    initial begin
-        // Initialize inputs
-        reset = 1;
-        key = 8'h00;
-        nonce = 2'b00;
-        plain_text_input1 = 1'b0;
-        plain_text_input2 = 1'b1;
-        lock = 0;
-        init_value = 2'b00;
-
-        // Dump waveform data
-        $dumpfile("main_tb.vcd");
-        $dumpvars(0, main_tb);
-
-        // Apply test vectors
-        #10 reset = 0; key = 8'b11011011; nonce = 2'b11; plain_text_input1 = 1'b1;plain_text_input2 = 1'b0; lock = 1;init_value = 2'b01;
-        #20 lock = 0;
-        #30 key = 8'b00000000; nonce = 2'b10;init_value = 2'b10;
-        #40 key = 8'b00110011; nonce = 2'b01; lock = 1; init_value = 2'b11;
-        #50 reset = 1;
-        #10 reset = 0; key = 8'b11111111; nonce = 2'b00; plain_text_input1 = 1'b0; plain_text_input2 = 1'b1;
-
-        // Finish simulation
-        #100 $finish;
-    end
-
-    initial begin
-        // Monitor the outputs
-        $monitor("At time %t, key = %b, nonce = %b, plain_text_input1 = %b,plain_text_input2 = %b, lock = %b, counter_init_value = %b, final_output = %b",
-                 $time, key, nonce, plain_text_input1,plain_text_input2, lock, init_value,final_output);
-    end
 endmodule
---
+++ Verilog/ChaCha_tb.v
@@ -0,0 +1,74 @@
+//testbench for main module
+module main_tb;
+    // Inputs
+    reg clk;
+    reg reset;
+    reg [7:0] key;
+    reg [1:0] nonce;
+    reg  plain_text_input1;
+    reg plain_text_input2;
+    reg lock;
+    reg [1:0] init_value;
+
+    // Outputs
+    wire final_output;
+
+    // Instantiate the main module
+    main uut (
+        .clk(clk),
+        .reset(reset),
+        .key(key),
+        .nonce(nonce),
+        .plain_text_input1(plain_text_input1),
+        .plain_text_input2(plain_text_input2),
+        .init_value(init_value),
+        .lock(lock),
+        .final_output(final_output)
+    );
+
+    // Clock generation
+    initial begin
+        clk = 0;
+        forever #5 clk = ~clk; // 10ns period clock
+    end
+
+    // Test sequence
+    initial begin
+        // Initialize inputs
+        reset = 1;
+        key = 8'h00;
+        nonce = 2'b00;
+        plain_text_input1 = 1'b0;
+        plain_text_input2 = 1'b0;
+        lock = 0;
+        init_value = 2'b00;
+
+        // Dump waveform data
+        $dumpfile("ChaCha.vcd");
+        $dumpvars(0, main_tb);
+
+        // Apply test vectors
+        #10 reset=0;
+        #10 key = 8'b11011011; nonce = 2'b11; lock = 1;init_value = 2'b01;
+        #10 lock = 0;
+        #10 plain_text_input1 = 1'b1;
+        #10 plain_text_input1 = 1'b0;
+        #10 plain_text_input1 = 1'b1;
+        #10 plain_text_input1 = 1'b0;
+        #10 plain_text_input1 = 1'b1;
+        #10 plain_text_input1 = 1'b0;
+
+        // #50 reset = 1;
+        #10 reset = 0; key = 8'b11011011; nonce = 2'b00; plain_text_input1 = 1'b0;;
+
+        // Finish simulation
+        #100 $finish;
+    end
+
+    initial begin
+        // Monitor the outputs
+        $monitor("At time %t, key = %b, nonce = %b, plain_text_input1 = %b,plain_text_input2 = %b, lock = %b, counter_init_value = %b, final_output = %b",
+                 $time, key, nonce, plain_text_input1,plain_text_input2, lock, init_value,final_output);
+    end
+endmodule
+
